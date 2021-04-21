8914ecc3216c034f453dcc5f794b3a2a47e8be64
chepy223 <anthonygonzalez.3@hotmail.com>
2021-04-21 13:48:40 +0000

    js

--- src/app.js
+++ src/app.js
@@ -9,3 +9,18 @@ window.onload = function() {
   //write your code here
   console.log("Hello Rigo from the console!");
 };
+
+let pronoun = ["the", "our"];
+let adj = ["great", "big"];
+let noun = ["jogger", "racoon"];
+let dom = [".com", ".uy"];
+
+for (let i = 0; i < pronoun.length; i++) {
+  for (let j = 0; j < adj.length; j++) {
+    for (let g = 0; g < noun.length; g++) {
+      for (let r = 0; r < dom.length; r++) {
+        console.log(pronoun[i] + adj[j] + noun[g] + dom[r]);
+      }
+    }
+  }
+}
